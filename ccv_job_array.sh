a637a2a52e6f1c2f7d3390dedaae1cfdf20e6e34
Dhananjay Bhaskar <dhananjay_bhaskar@brown.edu>
2020-03-29 22:31:55 -0400

    Use CCV job arrays to run multiple simulations simultaneously

---
+++ ccv_job_array.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -J MATLAB
+#SBATCH -t 23:00:00
+#SBATCH --array=1-5
+
+# Use '%A' for array-job ID, '%J' for job ID and '%a' for task ID
+#SBATCH -e arrayjob-%a.err
+#SBATCH -o arrayjob-%a.out
+
+echo "Starting job $SLURM_ARRAY_TASK_ID on $HOSTNAME"
+matlab-threaded -nodisplay -r "coculture_model($SLURM_ARRAY_TASK_ID); exit"
+module load anaconda/3-5.2.0
+source activate wong_lab
+python coculture_viz.py --job $SLURM_ARRAY_TASK_ID
---
+++ coculture_viz.py
@@ -0,0 +1,94 @@
+import os
+import math
+import numpy as np
+import pandas as pd
+import multiprocessing
+
+import matplotlib
+from matplotlib import cm
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+from optparse import OptionParser
+from joblib import Parallel, delayed
+
+parser = OptionParser()
+parser.add_option("-j", "--job", dest="jid", action = "store", type = "string", help = "run job", metavar = "JID")
+
+(options, args) = parser.parse_args()
+if options.jid:
+  job_id = options.jid
+else:
+  print("Error: Job ID not specified")
+  exit()
+
+def get_frame_data(sim_dir, itr):
+	c_iter = str(itr*100).zfill(6)
+	posMat = pd.read_csv(os.sep.join([sim_dir, 'Pos_'+c_iter+'.dat']), header=None)
+	position = [complex(pos.replace('i','j')) for pos in posMat.values[0]]
+	velMat = pd.read_csv(os.sep.join([sim_dir, 'Velocity_'+c_iter+'.dat']), header=None)
+	velocity = list()
+	for vel in velMat.values[0]:
+		if repr(vel)=='0':
+			velocity.append(complex('0+0j'))
+		else:
+			velocity.append(complex(vel.replace('i','j')))
+	nbdMat = pd.read_csv(os.sep.join([sim_dir, 'Neighbors_'+c_iter+'.dat']), header=None)
+	num_neighbors = [int(nbd) for nbd in nbdMat.values]
+	edges = None
+	if os.path.getsize(os.sep.join([sim_dir, 'EdgeX_'+c_iter+'.dat'])) > 0:
+		edgexMat = pd.read_csv(os.sep.join([sim_dir, 'EdgeX_'+c_iter+'.dat']), header=None)
+		edgeyMat = pd.read_csv(os.sep.join([sim_dir, 'EdgeY_'+c_iter+'.dat']), header=None)
+		edges = zip(list(edgexMat[0]),list(edgexMat[1]),list(edgeyMat[0]),list(edgeyMat[1]))
+	typeMat = pd.read_csv(os.sep.join([sim_dir, 'Types_'+c_iter+'.dat']), header=None)
+	cell_types = [int(ctype) for ctype in typeMat.values]
+	return (position, velocity, num_neighbors, edges, cell_types)
+
+def visualize_frame(sim_dir, itr, disp):
+	itr_s = str(itr).zfill(4)
+	data = get_frame_data(sim_dir, itr)
+	position = data[0]
+	velocity = data[1]
+	num_neighbors = data[2]
+	edges = data[3]
+	cell_types = data[4]
+	cdata_neighbors = list()
+	for nbd in num_neighbors:
+		if nbd == 0:
+			cdata_neighbors.append('indianred')
+		elif nbd < 4:
+			cdata_neighbors.append('royalblue')
+		else:
+			cdata_neighbors.append('seagreen')
+	cdata_types = list()
+	for ctype in cell_types:
+		if ctype == 1:
+			cdata_types.append('red')
+		elif ctype == 2:
+			cdata_types.append('green')
+		else:
+			cdata_types.append('blue')
+	plt.figure(figsize=(3,3), dpi=300)
+	plt.scatter(np.real(position), np.imag(position), color=cdata_types, s=8)
+	if edges is not None:
+		for e in edges:
+			plt.plot([e[0], e[1]], [e[2], e[3]], marker=None, linewidth=0.75, color='gray', alpha=0.15)
+	plt.xticks([])
+	plt.yticks([])
+	plt.xlim([-10.1, 10.1])
+	plt.ylim([-10.1, 10.1])
+	if disp:
+		plt.show()
+	else:
+		plt.savefig(sim_dir+'_plots'+os.sep+itr_s+'.png', bbox_inches='tight')
+	plt.close()
+
+sim_dir = 'DAH_Complete_Sorting_' + job_id
+#max_iter = 10000
+max_iter = 10
+os.mkdir(sim_dir+'_plots')
+
+num_cores = multiprocessing.cpu_count()
+print("Number of cores: " + repr(num_cores))
+
+Parallel(n_jobs=num_cores)(delayed(visualize_frame)(sim_dir, frame, False) for frame in range(0, max_iter+1, 2));
